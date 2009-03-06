65233f30ec2a6d6cfe13e167b7220530f168cfc7
Andrew vonderLuft <avonderluft@avlux.net>
2009-03-06 12:57:31 -0800

    updates; work in progress

--- db/migrate/001_add_page_options.rb
+++ db/migrate/001_add_page_options.rb
@@ -1,7 +1,7 @@
 class AddPageOptions < ActiveRecord::Migration
   def self.up   
     add_column :pages, :cache_expire_minutes, :integer, :allow_nil => false, :default => 0
-    add_column :pages, :cache_expire_time, :datetime, :default => nil  
+    add_column :pages, :cache_expire_time, :time, :default => nil  
   end
 
   def self.down
