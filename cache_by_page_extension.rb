9b99dae163ac8369941b32021987c2ce6a4383df
Andrew vonderLuft <avonderluft@avlux.net>
2011-05-09 13:32:27 -0700

    update version

--- cache_by_page_extension.rb
+++ cache_by_page_extension.rb
@@ -2,7 +2,7 @@
 # require_dependency 'application_controller'
 
 class CacheByPageExtension < Radiant::Extension
-  version "1.0"
+  version "#{File.read(File.expand_path(File.dirname(__FILE__)) + '/VERSION')}"
   description "Enables per page option of setting cache expire time, or turning off caching for a single page"
   url "https://github.com/avonderluft/radiant-cache_by_page-extension"
 
